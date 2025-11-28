c65e8cc6dab80a18994435b0184a01c92ffc2053
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-28 10:42:23 -0300

    Obter categoria por id

--- src/main/java/io/github/aplaraujo/controllers/CategoryController.java
+++ src/main/java/io/github/aplaraujo/controllers/CategoryController.java
@@ -1,26 +1,32 @@
 package io.github.aplaraujo.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.aplaraujo.entities.Category;
+import io.github.aplaraujo.controllers.dto.CategoryDTO;
+import io.github.aplaraujo.controllers.mappers.CategoryMapper;
+import io.github.aplaraujo.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+
 
 @RestController
 @RequestMapping(value="/categories")
+@RequiredArgsConstructor
 public class CategoryController {
 
-    @GetMapping
-    public List<Category> listar() {
-        Category cat1 = new Category(1L, "Informática");
-        Category cat2 = new Category(2L, "Escritório");
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
 
-        List<Category> list = new ArrayList<>();
-        list.add(cat1);
-        list.add(cat2);
-        return list;
+    @GetMapping(value="/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable("id") String id) {
+        var categoryId = Long.parseLong(id);
+        return categoryService.findById(categoryId).map(cat -> {
+            CategoryDTO dto = categoryMapper.toDTO(cat);
+            return ResponseEntity.ok(dto);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    
 }
--- src/main/java/io/github/aplaraujo/services/CategoryService.java
+++ src/main/java/io/github/aplaraujo/services/CategoryService.java
@@ -1,10 +1,21 @@
 package io.github.aplaraujo.services;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import io.github.aplaraujo.entities.Category;
+import io.github.aplaraujo.repositories.CategoryRepository;
+import io.github.aplaraujo.validators.CategoryValidator;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryValidator categoryValidator;
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
 }
